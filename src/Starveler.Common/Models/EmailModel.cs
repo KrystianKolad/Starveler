48b7e8863ba6fe834fbbbda72400d95433b1c8b7
Krystian <krystiankolad@wp.pl>
2018-03-17 13:01:37 +0100

    Cleaning up

--- src/Starveler.Common/Models/EmailModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Starveler.Common.Models
-{
-    public class EmailModel
-    {
-        public string Subject { get; set; }
-        public string Body { get; set; }
-        public string Recipient { get; set; }
-    }
-}
